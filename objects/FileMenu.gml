omicronrex/renex-engine

commit aa361edd71d984478ad2adbd16a287fc66dc8d2c

force reset save information on new file

scripts/savedata_select.gml
@@ -26,4 +26,7 @@ if (savedata("saved")) {
         }
         file_text_close(f)
     }
+} else {
+    global.statgridh=1
+    savedata_default()
 }

commit 7edea8dc7c5fab7dd8d183d3e348bd42614a4a1a

fix problem introduced in aa361edd

objects/FileMenu.gml
@@ -83,7 +83,6 @@ if (state="continue") {
 if (state="new file") {
     if (global.key_pressed[key_left] || global.key_pressed[key_right]) {
         sound_play("sndJump")
-        savedata_select(select)
         savedata("diff",modwrap(savedata("diff")+global.input_h,0,global.num_difficulties))
         difficulty=savedata("diff")
     }
@@ -91,8 +90,7 @@ if (state="new file") {
         instance_destroy_id(TitleParticle)
         instance_destroy_id(TitleMenu)
         input_clear()
-        savedata_select(select)
-        savedata_load()
+        savedata_newgame(difficulty)
     }
 }
 

scripts/index.yyd
@@ -138,3 +138,4 @@ camera_set
 camera_set_raw
 camera_shake
 camera_apply
+savedata_newgame

scripts/savedata_newgame.gml
@@ -1,0 +1,9 @@
+///savedata_newgame(difficulty)
+//creates a new savefile and starts it
+
+global.statgridh=1
+
+savedata_default()
+savedata("diff",argument0)
+
+savedata_load()

scripts/savedata_select.gml
@@ -26,7 +26,4 @@ if (savedata("saved")) {
         }
         file_text_close(f)
     }
-} else {
-    global.statgridh=1
-    savedata_default()
 }

scripts/tree.yyd
@@ -11,6 +11,7 @@
 	|settings_default
 	|savedata_default
 	|savedata_select
+	|savedata_newgame
 	|savedata_load
 	|savedata_save
 	|autosave_asap
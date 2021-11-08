omicronrex/renex-engine

commit 110a3d15c086aec12c8479c592cc6d4acd5f24b9

force reset save information on new file

scripts/savedata_select.gml
@@ -26,4 +26,7 @@ if (savedata("saved")) {
         }
         file_text_close(f)
     }
+} else {
+    global.statgridh=1
+    savedata_default()
 }

commit 9eb5f66ec574dbb2d309157146129128251890ac

fix problem introduced in 110a3d15

objects/FileMenu.gml
@@ -91,8 +91,7 @@ if (state="new file") {
         instance_destroy_id(TitleParticle)
         instance_destroy_id(TitleMenu)
         input_clear()
-        savedata_select(select)
-        savedata_load()
+        savedata_newgame(select)
     }
 }
 

scripts/index.yyd
@@ -133,3 +133,4 @@ camera_set
 camera_set_raw
 camera_shake
 camera_apply
+savedata_newgame

scripts/savedata_newgame.gml
@@ -1,0 +1,18 @@
+///savedata_newgame(slot)
+//creates a new savefile and starts it
+
+//grab current difficulty from blank savefile (FileMenu sets this)
+difficulty=savedata("diff")
+
+//clear it out
+savedata_select(argument0)
+savedata_default()
+
+//set diff
+savedata("diff",difficulty)
+
+//reset stats
+global.statgridh=1
+
+//go
+savedata_load()

scripts/savedata_select.gml
@@ -26,7 +26,4 @@ if (savedata("saved")) {
         }
         file_text_close(f)
     }
-} else {
-    global.statgridh=1
-    savedata_default()
 }

scripts/tree.yyd
@@ -10,6 +10,7 @@
 	|settings_default
 	|savedata_default
 	|savedata_select
+	|savedata_newgame
 	|savedata_load
 	|savedata_save
 	|autosave_asap